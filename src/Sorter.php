aaa013f56be138dbd6e8836e8b92ac2ac7ee1f6c
Andrii Lebediev <andrii.lebediev@bank.gov.ua>
2019-10-21 15:15:37 +0300

    Add chnageStrategy method

--- src/Sorter.php
+++ src/Sorter.php
@@ -13,6 +13,21 @@ class Sorter
 
     public function sort(array $sortArray): array
     {
-        return $this->sorter->sort($sortArray);
+        if (\array_keys($sortArray) !== \range(0, \count($sortArray) - 1)) {
+            $arrayType = 'associative';
+        } else {
+            $arrayType = null;
+        }
+
+        return $this->sorter->sort($sortArray, $arrayType);
     }
+
+    public function changeStrategy(SorterInterface $sorter): self
+    {
+        $this->sorter = $sorter;
+
+        return $this;
+    }
+
+
 }
